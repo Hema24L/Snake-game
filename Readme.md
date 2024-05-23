277a39236817c54c73f0c33966ac67030b91d1fd
Hemalatha <98384239+Hema24L@users.noreply.github.com>
2024-05-23 13:11:38 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,23 @@
+#Snake-game
+
+This is a classic snake game implemented in python. The objective is to control a snake that grows longer by eating food items scattered on the screen. Avoid hitting the walls or your own tail, or the game ends!
+
+## Features
+* Simple and intuitive gameplay
+* Customizable game speed
+* Score tracking
+
+## How to play
+
+* The snake starts with a short body and will grow longer each time it eats a food item.
+* Food items appear randomly on the screen.
+* The game ends when the snake collides with the walls or its own body.
+* Your score is displayed on the screen and increases with each eaten food item.
+
+## Controls
+Arrow keys (up, down, left, right): Change the direction of the snake.
+
+## How to run the game:
+1. Make sure you have Python 3.x installed.
+2. Open a terminal in the directory containing the game script (main.py)
+3. Run the script using the command `python main.py `
---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+# initializing the screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('snake game')
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+# Key controls
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is = True
+while game_is:
+    screen.update()
+    time.sleep(0.1)
+    # Moving the snake
+    snake.move()
+
+    # Snake collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+    
+    #detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -280:
+        game_is = False
+        score.game_over()  
+
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is = False
+            score.game_over()
+   
+screen.exitonclick()
